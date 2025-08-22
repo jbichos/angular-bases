beacf159c09995406cc556ba3635f034bbb54390
jbichos <jbichos@hotmail.com>
2025-08-22 10:03:28 +0200

    Modulo Heroes

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,22 +2,18 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
-// import { CounterComponent } from './counter/components/counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero';
-import { ListComponent } from './heroes/list/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // CounterComponent,
-    HeroComponent,
-    ListComponent,
   ],
   imports: [
     BrowserModule,
     CounterModule,
+    HeroesModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
