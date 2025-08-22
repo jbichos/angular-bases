53ef5d64bb291891117926f79e2d6ec26520f3c0
jbichos <jbichos@hotmail.com>
2025-08-22 10:45:26 +0200

    Módulo DBZ (Dragon Ball Z)

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  standalone: false,
+  templateUrl: './main-page.component.html',
+  // styleUrls: ['./main-page.component.css']
+})
+
+export class MainPageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
